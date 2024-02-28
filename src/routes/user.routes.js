049bd2115f22bb04790648fd4ad0d514179da116
AlaminHasanPro <alaminhasanpro@gmail.com>
2024-02-28 21:19:13 +0600

    authmiddleware fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,58 +1,3 @@
-// // user.routes.js
-// import { Router } from "express";
-// import passport from "passport";
-// import {
-//   deleteUser,
-//   getAllUsers,
-//   getCurrentUser,
-//   getSingleUser,
-//   loginUser,
-//   loginWithGoogle,
-//   logout,
-//   makeAdmin,
-//   refreshAccessToken,
-//   registerUser,
-// } from "../controllers/user.controller.js";
-// import { upload } from "../middlewares/multer.middleware.js";
-// import { verifyJWT } from "../middlewares/auth.middleware.js";
-// import { isAdmin } from "../middlewares/isAdmin.middleware.js";
-// const router = Router();
-
-// router.route("/register").post(
-//   upload.fields([
-//     { name: "avatar", maxCount: 1 },
-//     { name: "coverImage", maxCount: 1 },
-//   ]),
-//   registerUser
-// );
-
-// router.route("/login").post(loginUser);
-
-// router
-//   .route("/auth/google")
-//   .get(passport.authenticate("google", { scope: ["profile", "email"] }));
-// router
-//   .route("/auth/google/callback")
-//   .get(passport.authenticate("google", { failureRedirect: "/" }), (_, res) =>
-//     res.redirect("/")
-//   );
-
-// // secured routes
-// router.route("/logout").post(verifyJWT, logout);
-// router.route("/refreshToken").post(refreshAccessToken);
-// router.route("/currentUser").get(verifyJWT, getCurrentUser);
-// // router.route("/auth/google").post(loginWithGoogle);
-// // router.post("/admin-only-route", verifyJWT, isAdmin, (req, res) => {
-// //   res.json({ message: "This route is accessible only to admins." });
-// // });
-// router.route("/make-admin/:userId").put(verifyJWT, isAdmin, makeAdmin);
-// router.route("/getAllUser").get(verifyJWT, isAdmin, getAllUsers);
-// router.route("/getSingleUser/:userId").get(verifyJWT, getSingleUser);
-// router.route("/deleteUser/:userId").delete(verifyJWT, isAdmin, deleteUser);
-// export default router;
-
-
-
 import express from "express";
 import passport from "passport";
 import {
@@ -65,7 +10,7 @@ import {
   getSingleUser,
   deleteUser,
   registerUser,
-  loginUser,
+loginUser,
   logout,
   refreshAccessToken,
   getCurrentUser,
@@ -89,10 +34,10 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 
 router.route("/auth/google").get(googleAuthMiddleware);
-router.route("/auth/google/callback").get(
-  googleAuthCallbackMiddleware,
-  handleGoogleAuthCallback
-);router.route("/loginWithGoogle").post(loginWithGoogle);
+router
+  .route("/auth/google/callback")
+  .get(googleAuthCallbackMiddleware, handleGoogleAuthCallback);
+router.route("/loginWithGoogle").post(loginWithGoogle);
 
 router.route("/logout").post(verifyJWT, logout);
 router.route("/refreshToken").post(refreshAccessToken);
